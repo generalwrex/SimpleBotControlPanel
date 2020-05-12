ffa1d2944f49c060bc77fa9550faac0a66c11e52
Trapss <duttyhef@gmail.com>
2020-05-12 12:22:20 -0500

    formatting

--- botcp.js
+++ botcp.js
@@ -1,29 +1,29 @@
 // BotCP by General Wrex
 // edited by trq
-var forever = require('forever-monitor');
-var express = require("express");
-var bodyParser = require("body-parser");
-var app = express();
+var forever = require('forever-monitor')
+var express = require("express")
+var bodyParser = require("body-parser")
+var app = express()
 
-app.use(bodyParser.json());
+app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({
   extended: true
-}));
+}))
 
 app.get('/', function(req, res) {
   res.sendFile('index.html', {
     root: __dirname
-  });
-});
+  })
+})
 
-var child = null;
-var isRunning = false;
-let bPath;
-let bStart;
+var child = null
+var isRunning = false
+let bPath
+let bStart
 
 app.post('/start', function(req, res) {
-  bPath = req.body.path;
-  bStart = req.body.startup;
+  bPath = req.body.path
+  bStart = req.body.startup
 
   if (!child && bPath) {
     child = new(forever.Monitor)(bStart, {
@@ -34,38 +34,38 @@ app.post('/start', function(req, res) {
       watchDirectory: bPath,
       cwd: bPath,
       args: []
-    });
+    })
 
     child.on('exit', function() {
-      console.log(bStart + ' has exited');
-    });
+      console.log(bStart + ' has exited')
+    })
 
     child.on('error', function(error) {
-      console.log('Error! ' + error);
-    });
+      console.log('Error! ' + error)
+    })
   }
 
   if (child && !isRunning) {
     try {
-      child.start();
-      isRunning = true;
+      child.start()
+      isRunning = true
     } catch (error) {
-      console.log('Error! ' + error);
+      console.log('Error! ' + error)
     }
 
   }
 
-  res.send('done');
-});
+  res.send('done')
+})
 
 app.post('/stop', function(req, res) {
   if (child && isRunning) {
-    child.stop();
-    isRunning = false;
+    child.stop()
+    isRunning = false
   }
-  res.send('done');
-});
+  res.send('done')
+})
 
 var server = app.listen(3000, function() {
-  console.log("Listening on port %s...", server.address().port);
-});
+  console.log("Listening on port %s...", server.address().port)
+})
